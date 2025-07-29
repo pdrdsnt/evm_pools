d723b98b6ccbf6ed627d3436baae77f8183de018
pedro de santi <pdrodsnt@gmail.com>
2025-07-29 06:33:26 -0300

    refactoring trade function

--- src/v3_base/states.rs
+++ src/v3_base/states.rs
@@ -7,7 +7,7 @@ use alloy::primitives::{
 
 use crate::v3_base::{bitmap::BitMap, ticks::Ticks};
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Tick {
     pub tick: I24,
     pub liquidity_net: Option<i128>,
@@ -29,12 +29,24 @@ pub struct TradeReceipt {
 pub struct TradeState {
     pub fee_amount: U256,
     pub amount_in: U256,
+    pub amount_out: U256,
     pub liquidity: U256,
     pub x96price: U256,
     pub tick: I24,
     pub remaining: U256,
     pub from0: bool,
+    pub step: TradeStep,
 }
+#[derive(Debug, Clone, Copy, Default)]
+pub struct TradeStep {
+    pub amount_possible: U256,
+    pub next_tick: Tick,
+    pub current_tick_index: usize,
+    pub next_tick_index: usize,
+    pub next_price: U256,
+    pub delta: U256,
+}
+
 pub struct PoolState {
     pub current_tick: I24,
     pub ticks: Ticks,
--- src/v3_base/trade_math.rs
+++ src/v3_base/trade_math.rs
@@ -1,10 +1,14 @@
-use alloy::primitives::{U256, U512, aliases::U24};
+use alloy::primitives::{
+    U256, U512,
+    aliases::{I24, U24},
+};
 
 use crate::{
     err::{MathError, TickError, TradeError},
     v3_base::{
-        states::{PoolState, TradeState},
+        states::{PoolState, Tick, TradeState, TradeStep},
         tick_math::{price_from_tick, tick_from_price},
+        ticks::Ticks,
         x96price_math::{
             compute_amount_possible, compute_price_from0, compute_price_from1,
         },
@@ -17,239 +21,79 @@ pub fn trade(
     amount_in: U256,
     from0: bool,
 ) -> Result<TradeState, TradeError> {
-    let mut trade_state = TradeState {
-        fee_amount: U256::ZERO,
-        remaining: amount_in.clone(),
-        x96price: pool.x96price,
-        liquidity: pool.liquidity,
-        amount_in: amount_in,
-        tick: pool.current_tick,
-        from0: from0,
-    };
-    let fee_amount = amount_in
-        .checked_mul(U256::from(*fee))
-        .ok_or(MathError::A(trade_state))?
-        .checked_div(U256::from(1_000_000))
-        .ok_or(MathError::A(trade_state))?;
-    let mut remaining = amount_in
-        .checked_sub(fee_amount)
-        .ok_or(MathError::A(trade_state))?;
-    println!("remaining after fee {}", remaining);
-
-    trade_state.fee_amount = fee_amount;
-
-    // 2. Local state
-    let mut total_out = U256::ZERO;
-
-    trade_state.x96price = pool.x96price;
-
-    trade_state.tick = tick_from_price(pool.x96price).ok_or(MathError::A(trade_state))?;
-
-    let mut next_tick_index = match pool.ticks.get_tick_index(trade_state.tick) {
-        Ok(i) => {
-            if from0 {
-                if i + 1 >= pool.ticks.len() {
-                    return Err(TickError::Overflow(trade_state).into());
-                } // No ticks above
-                i + 1
-            } else {
-                if i == 0 {
-                    return Err(TickError::Underflow(trade_state).into());
-                } // No ticks below
-                i - 1
+    let mut trade_state = trade_start(pool, fee, amount_in, from0)?;
+    while trade_state.remaining > U256::ZERO {
+        trade_state = step_start(trade_state, &pool.ticks)?;
+        if trade_state.remaining < trade_state.step.amount_possible {
+            match handle_non_crossing_step(trade_state) {
+                Ok(ts) => trade_state = ts,
+                Err(err) => return Err(err),
             }
-        }
-        Err(i) => {
-            if from0 {
-                if i >= pool.ticks.len() {
-                    return Err(TickError::Overflow(trade_state).into());
-                } // No ticks above
-                i
-            } else {
-                if i == 0 {
-                    return Err(TickError::Underflow(trade_state).into());
-                } // No ticks below
-                i - 1
-            }
-        }
-    };
-    trade_state.liquidity = pool.liquidity;
-
-    while remaining > U256::ZERO {
-        // grab the next tick struct
-        let next_tick = pool
-            .ticks
-            .get(next_tick_index as usize)
-            .ok_or(MathError::A(trade_state))?; //wrong error
-
-        if next_tick.liquidity_net.is_none() {
-            return Err(TickError::Unavailable(trade_state).into());
-        }
-        // calculate the next tick’s price
-        let next_price =
-            price_from_tick(next_tick.tick).ok_or(MathError::A(trade_state))?;
-
-        // calculate the current price (based on current_tick)
-        let current_price_v =
-            price_from_tick(trade_state.tick).ok_or(MathError::A(trade_state))?;
-        let old_tick_index = next_tick_index;
-        next_tick_index = if from0 {
-            next_tick_index
-                .checked_add(1)
-                .ok_or(TickError::Overflow(trade_state))?
-        } else {
-            next_tick_index
-                .checked_sub(1)
-                .ok_or(TickError::Underflow(trade_state))?
-        };
-        // compute max amount possible to cross this tick
-        let possible =
-            compute_amount_possible(from0, &curr_liq, &curr_price, &next_price)
-                .ok_or(MathError::A(trade_state))?;
-
-        if remaining < possible {
-            // won't cross full tick
-            let new_price = if from0 {
-                compute_price_from0(&remaining, &curr_liq, &curr_price, true)
-                    .ok_or(MathError::A(trade_state))?
-            } else {
-                compute_price_from1(&remaining, &curr_liq, &curr_price, true)
-                    .ok_or(MathError::A(trade_state))?
-            };
-
-            // **DEBUG PRINT**
-            println!(
-                "[DEBUG] crossing partial tick: remaining={} < possible={}, new_price={}",
-                remaining, possible, new_price
-            );
-
-            let u512_curr_price = U512::from(curr_price);
-            let u512_curr_liq = U512::from(curr_liq);
-
-            // compute out
-            let delta = if from0 {
-                let price_diff = u512_curr_price
-                    .checked_sub(U512::from(new_price))
-                    .ok_or(MathError::A(trade_state))?;
-                println!("diff {}", price_diff);
-                u512_curr_liq
-                    .checked_mul(price_diff)
-                    .ok_or(MathError::A(trade_state))?
-                    .checked_div(U512::ONE << 96)
-                    .ok_or(MathError::A(trade_state))?
-            } else {
-                let inv_curr = (U512::ONE << U512::from(96_u32))
-                    .checked_mul(U512::ONE << 96)
-                    .ok_or(MathError::A(trade_state))?
-                    .checked_div(u512_curr_price)
-                    .ok_or(MathError::A(trade_state))?;
-                let inv_new = (U512::ONE << U512::from(96_u32))
-                    .checked_mul(U512::ONE << 96)
-                    .ok_or(MathError::A(trade_state))?
-                    .checked_div(U512::from(new_price))
-                    .ok_or(MathError::A(trade_state))?;
-                u512_curr_liq
-                    .checked_mul(
-                        inv_curr
-                            .checked_sub(inv_new)
-                            .ok_or(MathError::A(trade_state))?,
-                    )
-                    .ok_or(MathError::A(trade_state))?
-                    .checked_div(U512::from(1u128 << 96))
-                    .ok_or(MathError::A(trade_state))?
-            };
-
-            println!("delta {}", delta);
-
-            total_out = total_out
-                .checked_add(U256::from(delta))
-                .ok_or(MathError::A(trade_state))?;
-            remaining = U256::ZERO;
-            curr_price = U256::from(new_price);
-            // **DEBUG PRINT**
-            println!(
-                "[DEBUG] partial-cross: delta_out={} | total_out={} | curr_price(updated)={} | remaining=0",
-                delta, total_out, curr_price
-            );
-
-            trade_state.x96price = curr_price;
-            trade_state.liquidity = curr_liq;
-            trade_state.remaining = remaining;
-
             break;
         }
 
         // cross entire tick
         let out_cross = if from0 {
-            curr_liq
+            trade_state
+                .liquidity
                 .checked_mul(
-                    next_price
-                        .checked_sub(curr_price)
+                    trade_state
+                        .step
+                        .next_price
+                        .checked_sub(trade_state.x96price)
                         .ok_or(MathError::A(trade_state))?,
                 )
                 .ok_or(MathError::A(trade_state))?
                 .checked_div(U256::from(1u128 << 96))
                 .ok_or(MathError::A(trade_state))?
         } else {
-            let num = curr_liq
+            let num = trade_state
+                .liquidity
                 .checked_mul(
-                    curr_price
-                        .checked_sub(next_price)
+                    trade_state
+                        .x96price
+                        .checked_sub(trade_state.step.next_price)
                         .ok_or(MathError::A(trade_state))?,
                 )
                 .ok_or(MathError::A(trade_state))?;
             num.checked_div(U256::from(1u128 << 96))
                 .ok_or(MathError::A(trade_state))?
         };
-        total_out = total_out
+        trade_state.amount_out = trade_state
+            .amount_out
             .checked_add(out_cross)
             .ok_or(MathError::A(trade_state))?;
-
-        // **DEBUG PRINT**
-        println!(
-            "[DEBUG] full-cross: out_cross={} | total_out(before_liq_update)={} ",
-            out_cross,
-            total_out - out_cross
-        );
-
         // update liquidity
-        if let Some(net) = next_tick.liquidity_net {
-            let old_liq = curr_liq;
-            curr_liq = if from0 {
+        if let Some(net) = trade_state.step.next_tick.liquidity_net {
+            trade_state.liquidity = if from0 {
                 if net > 0 {
-                    curr_liq.saturating_add(U256::from(net))
+                    trade_state
+                        .liquidity
+                        .saturating_add(U256::from(net))
                 } else {
-                    curr_liq.saturating_sub(U256::from(-net))
+                    trade_state
+                        .liquidity
+                        .saturating_sub(U256::from(-net))
                 }
             } else {
                 if net < 0 {
-                    curr_liq.saturating_add(U256::from(net))
+                    trade_state
+                        .liquidity
+                        .saturating_add(U256::from(net))
                 } else {
-                    curr_liq.saturating_sub(U256::from(net))
+                    trade_state
+                        .liquidity
+                        .saturating_sub(U256::from(net))
                 }
             };
-            // **DEBUG PRINT**
-            println!(
-                "[DEBUG] liquidity_net={} | liquidity: {} → {}",
-                net, old_liq, curr_liq
-            );
         }
 
         // move pointer
-        curr_price = next_price;
-        remaining = remaining
-            .checked_sub(possible)
+        trade_state.x96price = trade_state.step.next_price;
+        trade_state.remaining = trade_state
+            .remaining
+            .checked_sub(trade_state.step.amount_possible)
             .ok_or(MathError::A(trade_state))?;
-
-        // **DEBUG PRINT**
-        println!(
-            "[DEBUG] end-of-iteration: curr_price={} | remaining={} | total_out={}\n",
-            curr_price, remaining, total_out
-        );
-
-        trade_state.x96price = curr_price;
-        trade_state.liquidity = curr_liq;
-        trade_state.remaining = remaining;
     }
 
     // build Trade
@@ -257,8 +101,8 @@ pub fn trade(
 }
 
 //pub fn trade_step(trade_state: TradeState) -> Result<TradeState, TradeError> {}
-pub fn handle_non_cossing_step(
-    trade_state: &mut TradeState,
+pub fn handle_non_crossing_step(
+    mut trade_state: TradeState,
 ) -> Result<TradeState, TradeError> {
     // won't cross full tick
     let new_price = if trade_state.from0 {
@@ -276,22 +120,16 @@ pub fn handle_non_cossing_step(
             &trade_state.x96price,
             true,
         )
-        .ok_or(MathError::A(trade_state.clone()))?
+        .ok_or(MathError::A(trade_state))?
     };
 
-    // **DEBUG PRINT**
-    println!(
-        "[DEBUG] crossing partial tick: remaining={} < possible={}, new_price={}",
-        remaining, possible, new_price
-    );
-
-    let u512_curr_price = U512::from(curr_price);
-    let u512_curr_liq = U512::from(curr_liq);
+    let u512_curr_price = U512::from(trade_state.x96price);
+    let u512_curr_liq = U512::from(trade_state.liquidity);
 
     // compute out
-    let delta = if from0 {
-        let price_diff = u512_curr_price
-            .checked_sub(U512::from(new_price))
+    let delta = if trade_state.from0 {
+        let price_diff = U512::from(new_price)
+            .checked_sub(u512_curr_price)
             .ok_or(MathError::A(trade_state))?;
         println!("diff {}", price_diff);
         u512_curr_liq
@@ -321,19 +159,104 @@ pub fn handle_non_cossing_step(
             .ok_or(MathError::A(trade_state))?
     };
 
-    println!("delta {}", delta);
-
-    total_out = total_out
+    trade_state.amount_out = trade_state
+        .amount_out
         .checked_add(U256::from(delta))
         .ok_or(MathError::A(trade_state))?;
-    remaining = U256::ZERO;
-    curr_price = U256::from(new_price);
+    trade_state.remaining = U256::ZERO;
+    trade_state.x96price = U256::from(new_price);
 
-    // **DEBUG PRINT**
-    println!(
-        "[DEBUG] partial-cross: delta_out={} | total_out={} | curr_price(updated)={} | remaining=0",
-        delta, total_out, curr_price
-    );
+    Ok(trade_state)
+}
+pub fn step_start(
+    mut trade_state: TradeState,
+    ticks: &Ticks,
+) -> Result<TradeState, TradeError> {
+    let mut new_step = TradeStep::default();
+    trade_state.step.next_tick_index = match ticks.get_tick_index(trade_state.tick) {
+        Ok(i) => {
+            if trade_state.from0 {
+                if i + 1 >= ticks.len() {
+                    return Err(TickError::Overflow(trade_state).into());
+                } // No ticks above
+                i + 1
+            } else {
+                if i == 0 {
+                    return Err(TickError::Underflow(trade_state).into());
+                } // No ticks below
+                i - 1
+            }
+        }
+        Err(i) => {
+            if trade_state.from0 {
+                if i >= ticks.len() {
+                    return Err(TickError::Overflow(trade_state).into());
+                } // No ticks above
+                i
+            } else {
+                if i == 0 {
+                    return Err(TickError::Underflow(trade_state).into());
+                } // No ticks below
+                i - 1
+            }
+        }
+    };
 
-    trade_state
+    trade_state.step.next_tick = *ticks
+        .get(trade_state.step.next_tick_index)
+        .expect("checked above");
+
+    if trade_state.step.next_tick.liquidity_net.is_none() {
+        return Err(TickError::Unavailable(trade_state).into());
+    }
+    // calculate the next tick’s price
+    trade_state.step.next_price = price_from_tick(trade_state.step.next_tick.tick)
+        .ok_or(MathError::A(trade_state))?;
+
+    // compute max amount possible to cross this tick
+    trade_state.step.amount_possible = compute_amount_possible(
+        trade_state.from0,
+        &trade_state.liquidity,
+        &trade_state.x96price,
+        &trade_state.step.next_price,
+    )
+    .ok_or(MathError::A(trade_state))?;
+
+    Ok(trade_state)
+}
+
+pub fn trade_start(
+    pool: &PoolState,
+    fee: &U24,
+    amount_in: U256,
+    from0: bool,
+) -> Result<TradeState, TradeError> {
+    let mut trade_state = TradeState {
+        fee_amount: U256::ZERO,
+        remaining: amount_in.clone(),
+        amount_out: U256::ZERO,
+        x96price: pool.x96price,
+        liquidity: pool.liquidity,
+        amount_in: amount_in,
+        tick: pool.current_tick,
+        from0: from0,
+        step: TradeStep::default(),
+    };
+    let fee_amount = amount_in
+        .checked_mul(U256::from(*fee))
+        .ok_or(MathError::A(trade_state))?
+        .checked_div(U256::from(1_000_000))
+        .ok_or(MathError::A(trade_state))?;
+    trade_state.remaining = amount_in
+        .checked_sub(fee_amount)
+        .ok_or(MathError::A(trade_state))?;
+
+    trade_state.fee_amount = fee_amount;
+
+    trade_state.x96price = pool.x96price;
+
+    trade_state.tick = tick_from_price(pool.x96price).ok_or(MathError::A(trade_state))?;
+
+    trade_state.liquidity = pool.liquidity;
+    Ok(trade_state)
 }
